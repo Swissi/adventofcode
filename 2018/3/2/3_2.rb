1bd4c6c3a90a65d6bca35eebf9918ab8a16985ce
Dave <david.zahorsky@hotelplan.com>
2018-12-03 11:18:11 +0100

    3.2 done

--- 2018/3/2/3_2.rb
+++ 2018/3/2/3_2.rb
@@ -3,8 +3,8 @@ lines = File.readlines('input.txt')
 cloth = {}
 claims = 0
 
-
 lines.each do |line|
+  #1 @ 669,271: 17x11
   arr = line.split(' ')
   coord = arr[2]
   area = arr[3]
@@ -33,5 +33,34 @@ lines.each do |line|
   end
 end
 
-puts claims
+lines.each do |line|
+
+  #1 @ 669,271: 17x11
+  arr = line.split(' ')
+  id = arr[0]
+  coord = arr[2]
+  area = arr[3]
+  does_intersect = false
+
+  arr1 = coord.split(',')
+  x = Integer(arr1[0])
+  y = Integer(arr1[1].chomp(':'))
+
+  arr2 = area.split('x')
+  w = Integer(arr2[0])
+  l = Integer(arr2[1])
+
+  (1..w).each do |w|
+    (1..l).each do |l|
+      hash_key = (x + w).to_s + '_' + (y + l).to_s
+      if cloth[hash_key] > 1
+        does_intersect = true
+        break
+      end
+    end
+  end
+
+  puts id if does_intersect == false
+end
+
 
